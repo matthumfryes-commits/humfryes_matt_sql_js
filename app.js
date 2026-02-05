e45d0ebf84228886a1d598887122480f690424eb
unknown <matt.humfryes@gmail.com>
2026-02-05 16:11:21 +0100

    0.3.0 - Added 'INSERT' and 'UPDATE' functions for 'etudiant'

--- app.js
+++ app.js
@@ -41,8 +41,48 @@ export async function findStudentById(id) {
     }
 }
 
-await findAllStudents();
-await findStudentById(2);
+export async function addStudent(numCarte, nom, prenom, mail, annees, telephone, adresse1, adresse2, postal, ville, niveau, tuteurId) {
+    const conn = await pool.getConnection();
+    try 
+    {
+        await conn.beginTransaction();
+        await conn.query("INSERT INTO etudiant (etudiant_num_carte_identite, etudiant_nom, etudiant_prenom, etudiant_email, etudiant_annees_experience, etudiant_telephone, etudiant_adresse_ligne_1, etudiant_adresse_ligne_2, etudiant_code_postal, etudiant_ville, etudiant_niveau_etudes, tuteur_Id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",[numCarte, nom, prenom, mail, annees, telephone, adresse1, adresse2, postal, ville, niveau, tuteurId],);
+        await conn.commit();
+    } 
+    catch (err) 
+    {
+        await conn.rollback();
+        throw err;
+    } 
+    finally 
+    {
+        conn.release();
+    }
+}
+
+export async function updateStudent(mail, annees, telephone, adresse1, adresse2, postal, ville, niveau, id) {
+    const conn = await pool.getConnection();
+    try 
+    {
+        await conn.beginTransaction();
+        await conn.query("UPDATE etudiant SET etudiant_email = ?, etudiant_annees_experience = ?, etudiant_telephone = ?, etudiant_adresse_ligne_1 = ?, etudiant_adresse_ligne_2 = ?, etudiant_code_postal = ?, etudiant_ville = ?, etudiant_niveau_etudes = ? WHERE etudiant_Id = ?",[mail, annees, telephone, adresse1, adresse2, postal, ville, niveau, id],);
+        await conn.commit();
+    } 
+    catch (err) 
+    {
+        await conn.rollback();
+        throw err;
+    } 
+    finally 
+    {
+        conn.release();
+    }
+}
+
+//await findAllStudents();
+//await findStudentById(2);
+//await addStudent("CI-29007", "Humfryes", "Matt", "matt.humfryes@civelampus.com", 4, "0600000088", "9 rue Omega", null, 18282, "City", "Bac+3", 3);
+await updateStudent("matt.humfryes@civelampus.fr", 5, "0600000088", "9 rue Omega", null, 18282, "Town", "Bac+5", 10)
 
 export async function findAllCours() {
     let conn;
@@ -110,9 +150,9 @@ export async function updateCours(semaine, thematique, taux, heure, id) {
     }
 }
 
-await findCoursById(1);
-await findAllCours();
-await addCours(13, "GIT", 60, 14, 6, "11111111111111", 3);
-await updateCours(14, "Java", 65, 16, 11);
+//await findCoursById(1);
+//await findAllCours();
+//await addCours(13, "GIT", 60, 14, 6, "11111111111111", 3);
+//await updateCours(14, "Java", 65, 16, 11);
 
 pool.end();
