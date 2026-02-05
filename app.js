f4d565e041ede58f3fd46b269031386d6d838ffb
unknown <matt.humfryes@gmail.com>
2026-02-05 14:49:40 +0100

    0.1.0 - Added the connection to the database

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import mariadb from "mariadb";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+require('dotenv').config();
 
 const pool = mariadb.createPool({
-host: "localhost",
-user: "root",
-password: "Bowser2013",
-database: "civelampus",
-connectionLimit: 10,
+host: process.env.HOST,
+user: process.env.USER,
+password: process.env.PASSWORD,
+database: process.env.DATABASE,
+connectionLimit: process.env.CONNECTION_LIMIT,
 });
